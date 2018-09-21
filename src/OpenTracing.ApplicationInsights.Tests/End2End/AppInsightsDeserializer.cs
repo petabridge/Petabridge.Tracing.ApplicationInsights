e70a2aa72cd61b866113ec586924049ea3b75f53
Aaron Stannard <aaron@petabridge.com>
2018-09-21 11:00:35 -0500

    added JSON deserialization for responses back from the AppInsights API

---
+++ src/OpenTracing.ApplicationInsights.Tests/End2End/AppInsightsDeserializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OpenTracing.ApplicationInsights.Tests.End2End
+{
+    public static class AppInsightsDeserializer
+    {
+        public static AppInsightsResult DeserializeResult(string appInsightsJson)
+        {
+            return JsonConvert.DeserializeObject<AppInsightsResult>(appInsightsJson);
+        }
+    }
+
+    public class Column
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Table
+    {
+        public string name { get; set; }
+        public List<Column> columns { get; set; }
+        public List<List<object>> rows { get; set; }
+    }
+
+    public class AppInsightsResult
+    {
+        public List<Table> tables { get; set; }
+    }
+}
---
+++ src/OpenTracing.ApplicationInsights.Tests/End2End/AppInsightsDeserializerSpecs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace OpenTracing.ApplicationInsights.Tests.End2End
+{
+    /// <summary>
+    /// Sanity checks for ensuring that <see cref="AppInsightsDeserializer"/>
+    /// works as expected.
+    /// </summary>
+    public class AppInsightsDeserializerSpecs
+    {
+        private readonly string _sampleJson;
+
+        public AppInsightsDeserializerSpecs()
+        {
+            _sampleJson = File.ReadAllText("End2End/appInsightsResponse.json");
+        }
+
+        [Fact(DisplayName = "Should be able to deserialize AppInsights REST JSON")]
+        public void ShouldDeserializeValidAppInsightsJson()
+        {
+            var obj = AppInsightsDeserializer.DeserializeResult(_sampleJson);
+            obj.tables.Count.Should().Be(1);
+            obj.tables[0].rows.Count.Should().Be(4); // 4 ops in this JSON
+        }
+    }
+}
